31e23edb409d119d210377db867de4017e49f169
phanthanhtan <thanhtan.021192@gmail.com>
2015-01-10 16:56:22 +0700

    nothing

---
+++ ThanhThanhCong_test_webform/TTC_HopDongThueDat.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'F:\DCT\windows form\ThanhThanhCong_test_webform\ThanhThanhCong_test_webform\TTC_HopDongThueDat.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
